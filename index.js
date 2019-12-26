a28ac9a1a4f8a5a63221c50c27146f9e1b3b9bcb
Veronica <verookam@gmail.com>
2019-12-26 19:20:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let cvs = document.getElementById('canvas')
+let ctx = cvs.getContext('2d')
+
+let bird = new Image()
+let bg = new Image()
+let fg = new Image()
+let pipeTop = new Image()
+let pipeBottom = new Image()
+
